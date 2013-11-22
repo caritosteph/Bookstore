aed7157dd6df919a7dab98f7ca9b59040b0a2954
sadhu89 <carlos.sadhu@gmail.com>
2013-11-22 03:30:37 -0500

    cliente

--- application/controllers/admin/cliente.php
+++ application/controllers/admin/cliente.php
@@ -15,14 +15,25 @@ class Cliente extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
+        $this->load->model('admin/cliente_model');
     }
     
-    public function index() {
+    public function index($pag=0) {
+       
         $menu['activo']='cliente';
+        $datos['total']=  round($this->cliente_model->get_total()/POR_PAGINA);       
+        $datos['clientes']=  $this->cliente_model->get_clientes(isset($_GET['cliente'])?$_GET['cliente']:"", $datos['total'],$pag);
         
         $this->load->view('plantilla_admin/header',$menu);
-        $this->load->view('admin/cliente');
+        $this->load->view('admin/cliente', $datos);
         $this->load->view('plantilla_admin/footer');
+    }
+    
+    public function eliminar($id) {
+        $this->cliente_model->eliminar($id);
+        
+        $this->index();
+        
     }
 }
 
