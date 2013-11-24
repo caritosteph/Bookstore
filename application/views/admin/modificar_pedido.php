48a38134d0ff988d3c499af5cfbb90dc69e0c6fe
caritosteph <10200194@unmsm.edu.pe>
2013-11-24 15:36:54 -0500

    Administrador

--- application/views/admin/modificar_pedido.php
+++ application/views/admin/modificar_pedido.php
@@ -4,113 +4,118 @@
             <h3 class="panel-title"><span class="bold"><?=$titulo?></span></h3>
         </div>
         <div class="panel-body col-6">
-            <div class="text-center">
-                <label>Pedido ID:</label><span class="bold">  <?= $pedido->id?></span>
-            </div>
-            <div class="row marketing">
-                <div class="col-lg-6">
-                    <div>
-                        <label>Nombres</label>
-                        <input type="text" class="form-control" id="nombre" name="nombre" value="<?=$pedido->Nombre?>" required>
-                        <?php echo form_error('nombre'); ?>
-                    </div>
-                    <div>
-                        <label>Fecha del Pedido</label>
-                        <div id="datetimepicker" class="input-group">
-                            <input  data-format="yyyy-MM-dd" type="text" class="form-control" value="<?= $pedido->FechaPedido?>">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
-                            </span>
+            <form method="post" action="<?= base_url()?>admin/pedido/actualizar/<?= $pedido->id?>">
+                <div class="text-center">
+                    <label>Pedido ID:</label><span class="bold">  <?= $pedido->id?></span>
+                </div>
+                <div class="row marketing">
+                    <div class="col-lg-6">
+                        <div>
+                            <label>Nombres</label>
+                            <input type="text" class="form-control" id="nombre" name="nombre" value="<?=$pedido->Nombre?>" required>
+                            <?php echo form_error('nombre'); ?>
                         </div>
-                    </div>
-
-                </div>
-                <div class="col-lg-6">
-                    <div>
-                        <label>Apellidos</label>
-                        <input type="text" class="form-control" id="apellido" name="apellido" value="<?= $pedido->Apellidos?>" required>
-                        <?php echo form_error('apellido'); ?>
-                    </div>
-                    <div>
-                        <label>Fecha de Recogo</label>
-                        <div id="datetimepicker" class="input-group">
-                            <input  data-format="yyyy-MM-dd" type="text" class="form-control" value="<?= $pedido->FechaRecogo?>">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
-                            </span>
+                        <div>
+                            <label>Fecha del Pedido</label>
+                            <div id="datetimepicker" class="input-group">
+                                <input  data-format="yyyy-MM-dd" type="text" name="fecha" class="form-control" value="<?= $pedido->FechaPedido?>">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+                                </span>
+                            </div>
                         </div>
+
+                    </div>
+                    <div class="col-lg-6">
+                        <div>
+                            <label>Apellidos</label>
+                            <input type="text" class="form-control" id="apellido" name="apellido" value="<?= $pedido->Apellidos?>" required>
+                            <?php echo form_error('apellido'); ?>
+                        </div>
+                        <div>
+                            <label>Fecha de Recogo</label>
+                            <div id="datetimepicker" class="input-group">
+                                <input  data-format="yyyy-MM-dd" type="text" name ="fechar" class="form-control" value="<?= $pedido->FechaRecogo?>">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-calendar"></span></button>
+                                </span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="container col-lg-6 centrado1">
+                        <div class="form-group">
+                            <br><label>Estado</label>
+                            <select name="estado" class="form-control input" >
+                                <?php foreach ($estado as $e) { ?>
+                                    <option value="<?= $e->Estado?>" <?php
+                                    if (isset($pedido) && strnatcasecmp($e->Estado, $pedido->Estado) == 0) {
+                                        echo 'selected';
+                                    }
+                                    ?>><?= $e->Estado?></option>
+                                        <?php } ?>
+                            </select>
+                            <?php echo form_error('estado'); ?>
+                        </div><br>
                     </div>
 
                 </div>
-                <div class="container col-lg-6 centrado1">
-                    <div class="form-group">
-                        <br><label>Estado</label>
-                        <select name="estado" class="form-control input" >
-                            <?php foreach ($estado as $e) { ?>
-                                <option value="<?= $e->Estado?>" <?php
-                                if (isset($pedido) && strnatcasecmp($e->Estado, $pedido->Estado) == 0) {
-                                    echo 'selected';
-                                }
-                                ?>><?= $e->Estado?></option>
-                                    <?php } ?>
-                        </select>
-                        <?php echo form_error('estado'); ?>
-                    </div><br>
+                <div class="container">
+                    <table class="table table-bordered table-hover text-center">
+                        <thead>
+                            <tr>
+                                <th class="text-center">NOMBRE PRODUCTO</th>
+                                <th class="text-center">CANTIDAD</th>
+                                <th class="text-center">PRECIO</th>
+                                <th class="text-center">SUB TOTAL</th>
+                                <th class="text-center"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($libro as $lb) { ?>
+                                <tr>
+                                    <td class="text-center"><input class="form-control" type="text" name="titulo" value="<?= $lb->Titulo?>"></td>
+                                    <td class="text-center  col-md-1"><input class="form-control text-center" type="text" name="cantidad" value="<?= $lb->Unidades?>"></td>
+                                    <td class="text-center col-md-3">
+                                       <input type="text" class="form-control text-center" class="label label-primary" name="precio" value="<?= $lb->Precio?>">
+                                    </td>
+                                    <td class="text-center">
+                                        <span class="label label-default"><?= $lb->PrecioTotal?></span>
+                                    </td>
+                                    <td>
+                                        <a href="<?=base_url()?>admin/pedido/eliminarItemPedido/<?=$lb->id?>/<?= $pedido->id?>" class="btn btn-sm btn-danger text-center"><span class="glyphicon glyphicon-remove"></span></a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            <tr>
+                                <td colspan="3" class="text-right"><span class="bold">SUBTOTAL</span></td>
+                                <td class="text-center">
+                                    <span class="label label-success"><strong><?= $pedido->PrecioSinIGV?></strong></span>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3" class="text-right"><span class="bold">IGV</span></td>
+                                <td class="text-center">
+                                    <span class="label label-warning"><strong><?= $pedido->IGV?></strong></span>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="3" class="text-right"><span class="bold">TOTAL</span></td>
+                                <td class="text-center">
+                                    <span class="label label-danger"><?= $pedido->TotalCargo?></span>
+                                </td>
+                                <td></td>
+
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-
-            </div>
-            <div class="container">
-                <table class="table table-bordered table-hover text-center">
-                    <thead>
-                        <tr>
-                            <th class="text-center">NOMBRE PRODUCTO</th>
-                            <th class="text-center">CANTIDAD</th>
-                            <th class="text-center">PRECIO</th>
-                            <th class="text-center">SUB TOTAL</th>
-                            <th class="text-center"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                           <?php foreach ($libro as $lb) { ?>
-                        <tr>
-                            <td class="text-center"><input class="form-control" type="text" name="titulo" value="<?= $lb->Titulo?>"></td>
-                            <td class="text-center  col-md-1"><input class="form-control text-center" type="text" name="cantidad" value="<?= $lb->Unidades?>"></td>
-                            <td class="text-center">
-                                <span class="label label-primary"><?= $lb->Precio?></span>
-                            </td>
-                            <td class="text-center">
-                                <span class="label label-default"><?= $lb->PrecioTotal?></span>
-                            </td>
-                            <td>
-                                <a href="#"  class="btn btn-sm btn-danger text-center"><span class="glyphicon glyphicon-remove"></span></a>
-                            </td>
-                        </tr>
-                        <?php } ?>
-                        <tr>
-                            <td colspan="3" class="text-right"><span class="bold">SUBTOTAL</span></td>
-                            <td class="text-center">
-                                <span class="label label-success"><strong><?= $pedido->PrecioSinIGV?></strong></span>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td colspan="3" class="text-right"><span class="bold">IGV</span></td>
-                            <td class="text-center">
-                                <span class="label label-warning"><strong><?= $pedido->IGV?></strong></span>
-                            </td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td colspan="3" class="text-right"><span class="bold">TOTAL</span></td>
-                            <td class="text-center">
-                                <span class="label label-danger"><?= $pedido->TotalCargo?></span>
-                            </td>
-                            <td></td>
-
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                <div class="actions text-center">
+                    <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-floppy-saved"></span> Guardar</button>&nbsp;<a href="<?= base_url()?>admin/pedido"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-floppy-remove"></span> Cancelar</button></a>
+                </div><br>
+            </form>
         </div>
     </div>
 </div>
