2c3dd4c5069e63fcaa9c9c086e9f19f3787e2fc9
caritosteph <10200194@unmsm.edu.pe>
2013-11-24 00:14:48 -0500

    Administrador

--- application/views/admin/detalle_pedido.php
+++ application/views/admin/detalle_pedido.php
@@ -4,40 +4,64 @@
             <h3 class="panel-title"><span class="bold"><?=$titulo?></span></h3>
         </div>
         <div class="panel-body col-6">
-            <div class="container col-lg-6 centrado1">
-                <p>
-                    <label for="nombre">Pedido ID:</label><span class="bold"> <?= $pedido->id?></span>
-                </p>
-                <p>
-                    <label for="apellido">Cliente:</label> <?= ucwords($pedido->Nombre.' '.$pedido->Apellidos)?>
-                </p>
-                <p>
 
-                    <label for="email">Fecha del Pedido:</label> <?= $pedido->FechaPedido?>       
-                </p>
-                <p>
-                    <?php
-                    $estado = $pedido->Estado;
-                    switch ($estado) {
-                        case 'En proceso':$badge = 'info';
-                            break;
-                        case 'Suspendido':$badge = '';
-                            break;
-                        case 'Faltan existencias':$badge = 'important';
-                            break;
-                        case 'Entregado':$badge = 'success';
-                            break;
-                        case 'Impagado':$badge = 'falta';
-                            break;
-                        case 'Cancelado':$badge = 'cancel';
-                            break;
-                        default:$badge = 'other';
-                            break;
-                    }
-                    ?>
-                    <label for="email">Estado:</label>&nbsp;<span class="badge badge-<?=$badge?>"> <?= $pedido->Estado?></span> 
-                </p>
+            <div class="row marketing centradito">
+                <div class="col-lg-6">
+                    <div>
+                        <label>PEDIDO ID</label>
+                    </div>
+                    <div>
+                        <label>CLIENTE</label>
+                    </div>
+                    <div>
+                        <label>FECHA DEL PEDIDO</label>    
+                    </div>
+                    <div>
 
+                        <label>FECHA DE RECOGO</label>    
+                    </div>
+                    <div>
+
+                        <label>ESTADO</label>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div>
+                        <label><span class="bold"> <?= $pedido->id?></span></label>
+                    </div>
+
+                    <div>
+                        <label><?= strtoupper($pedido->Nombre.' '.$pedido->Apellidos)?></label>
+                    </div>
+                    <div>
+                        <label><?= $pedido->FechaPedido?></label> 
+                    </div>
+                    <div>
+                        <label><?= $pedido->FechaRecogo?></label>  
+                    </div>
+                    <div>
+                        <?php
+                        $estado = $pedido->Estado;
+                        switch ($estado) {
+                            case 'En proceso':$badge = 'info';
+                                break;
+                            case 'Suspendido':$badge = '';
+                                break;
+                            case 'Faltan existencias':$badge = 'important';
+                                break;
+                            case 'Entregado':$badge = 'success';
+                                break;
+                            case 'Impagado':$badge = 'falta';
+                                break;
+                            case 'Cancelado':$badge = 'cancel';
+                                break;
+                            default:$badge = 'other';
+                                break;
+                        }
+                        ?>
+                        <label><span class="badge badge-<?=$badge?>"> <?= $pedido->Estado?></span></label> 
+                    </div>
+                </div>
             </div>
             <div class="container">
                 <table class="table table-bordered table-hover text-center">
