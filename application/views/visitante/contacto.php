88754463028a14a455141025db6a037a33a147b0
caritosteph <10200194@unmsm.edu.pe>
2013-12-01 18:30:41 -0500

    Administrador

--- application/views/visitante/contacto.php
+++ application/views/visitante/contacto.php
@@ -1,36 +1,52 @@
 <div class="container">
-    <h2 class="text-center">CONTACTO</h2>
+    <div class="text-left">
+        <h3><img src="<?= base_url() ?>img/contacto.png"/><span class="bold"> CONTACTO</span></h3>
+        <hr>
+    </div>
+    <div class="col-sm-offset-1 col-md-10">
+        <div class="row marketing">
+            <form method="post" action="#" enctype="multipart/form-data">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingresa tu Nombre" required>
+                        <?php echo form_error('nombre'); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Correo Electrónico</label>
+                        <input type="email" class="form-control" name="email" placeholder="Ingresa tu Correo Electrónico" required>
+                        <?php echo form_error('email'); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="clave">Comentario</label>
+                        <textarea type="text" class="form-control" name="comentario" rows="5"></textarea>
+                        <?php echo form_error('comentario'); ?>
+                    </div>
+                    <div class="actions text-center">
+                        <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-floppy-saved"></span>Enviar</button>&nbsp;<a href="#"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-floppy-remove"></span> Cancelar</button></a>
+                    </div>
+                </div>
+                <div class="col-sm-offset-1 col-lg-5">
+                    <label>Directora:</label>
+                    <p>Mag. Emma Patricia Victorio Cánovas</p>
+                    <label>Jefe de Administración:</label>
+                    <p>Sra. Norma Alpún Villarroel</p>
+                    <label>Telefono principal:</label>
+                    <p>619-7000</p>
+                    <ul>
+                        <li>DIRECCIÓN: Anexo 7531</li>
+                        <li>ADMINISTRACIÓN: Anexo 7529/7531</li>
+                        <li>VENTAS: Anexo 7530/7534</li>
+                    </ul>
 
-        <div class="col-sm-offset-3 col-sm-6">
-            <div class="row marketing">
-                
-                <form method="post" action="#" enctype="multipart/form-data">
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                            <label for="nombre">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingresa tu Nombre" required>
-                            <?php echo form_error('nombre'); ?>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Correo Electrónico</label>
-                            <input type="email" class="form-control" name="email" placeholder="Ingresa tu Correo Electrónico" required>
-                            <?php echo form_error('email'); ?>
-                        </div>
-                        <div class="form-group">
-                            <label for="clave">Comentario</label>
-                            <textarea type="text" class="form-control" name="comentario" rows="5"></textarea>
-                            <?php echo form_error('comentario'); ?>
-                        </div>
-                        <div class="actions text-center">
-                            <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-floppy-saved"></span>Enviar</button>&nbsp;<a href="#"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-floppy-remove"></span> Cancelar</button></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        
-                    </div>
-                </form>
-            </div>
+                    <label>Direccion:</label>
+                    <p>Jr. Germán Amezaga S/N, Biblioteca Central
+                        "Pedro Zulen",Cuarto Piso</p>
+                    <p class="text-center">Ciudad Universitaria Lima - Perú</p>
+                </div>
+            </form>
         </div>
+    </div>
 
 
 </div>
--- application/views/visitante/nosotros.php
+++ application/views/visitante/nosotros.php
@@ -1,19 +1,28 @@
 <div class="row">
+
+    <div class="col-lg-offset-2 col-md-8">
+        <div class="text-left">
+            <img src="<?= base_url() ?>img/carrito.jpg"/>
+            <h3><span class="bold">NOSOTROS</span></h3>
+            <hr>
+        </div>
+    </div>
+
     <div class="col-lg-offset-2 col-lg-4">
         <img class="img-circle centradito" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url()?>img/vision.jpg"">
         <h2 class="centradito bold">&nbsp;&nbsp;&nbsp;VISION</h2>
         <p class="text-justified">Somos el Fondo Editorial de la Universidad Nacional Mayor de San Marcos, difusores del conocimiento cientíco, tecnológico,
-humanístico, artístico y cultural; responsables de la excelencia y cuidado de nuestras publicaciones en apoyo de investigadores
-y creadores sanmarquinos; promotores de valores, identidad nacional y cultura de calidad, comprometidos con el incremento y
-preservación del patrimonio bibliográco del Perú.</p>
-        
+            humanístico, artístico y cultural; responsables de la excelencia y cuidado de nuestras publicaciones en apoyo de investigadores
+            y creadores sanmarquinos; promotores de valores, identidad nacional y cultura de calidad, comprometidos con el incremento y
+            preservación del patrimonio bibliográco del Perú.</p>
+
     </div>
     <div class="col-lg-4">
         <img class="img-circle centradito" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url()?>img/mision.jpg"">
         <h2 class="centradito bold">&nbsp;&nbsp;&nbsp;MISION</h2>
         <p class="text-justified">Fondo Editorial de la Universidad Nacional Mayor de San Marcos, referente nacional e internacional en publicaciones de calidad
-de investigaciones de carácter humanístico, cientíco y tecnológico, líder en la divulgación cultural y artística.</p>
-        
+            de investigaciones de carácter humanístico, cientíco y tecnológico, líder en la divulgación cultural y artística.</p>
+
     </div>
 </div>
 <div class="row">
@@ -21,8 +30,8 @@ de investigaciones de carácter humanístico, cientíco y tecnológico, líder en la
         <img class="img-circle text-center centradito" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url()?>img/servicios.jpg"">
         <h2 class="centradito bold">SERVICIOS</h2>
         <p class="text-justified">Maquetación de libros, edición, corrección de estilos, diagramación de interiores, diseño de carátulas y
-aches. Supervisión editorial que garantiza la calidad de los materiales y la competitividad del producto; así como
-distribución y venta de libros de autores Sanmarquinos.</p>
-        
+            aches. Supervisión editorial que garantiza la calidad de los materiales y la competitividad del producto; así como
+            distribución y venta de libros de autores Sanmarquinos.</p>
+
     </div>
 </div>
