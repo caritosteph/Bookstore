4e37d1d15ceaa1b22571ad1def75030f2160f3e4
caritosteph <10200194@unmsm.edu.pe>
2013-12-01 01:47:59 -0500

    Administrador

--- application/views/plantilla/footer.php
+++ application/views/plantilla/footer.php
@@ -1,6 +1,6 @@
-       <div class="footer">
+       <div class="footer color2">
             <div class="container">
-                <p class="text-muted credit text-center">
+                <p class="text-muted credit text-center ">
                     <span class="bold">© Tienda Virtual 2013</span>
                     <a href="#" class="btn btn-default pull-right"><span class="glyphicon glyphicon-arrow-up"></span>Subir</a>
                 </p>
