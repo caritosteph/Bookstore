adde5224b65f2012123c9c42eea933e0f732af73
caritosteph <10200194@unmsm.edu.pe>
2013-12-02 10:37:23 -0500

    compra

--- application/views/visitante/contacto.php
+++ application/views/visitante/contacto.php
@@ -5,7 +5,7 @@
     </div>
     <div class="col-sm-offset-1 col-md-10">
         <div class="row marketing">
-            <form method="post" action="<?= base_url()?>contacto" enctype="multipart/form-data">
+            <form method="post" id="frm" action="<?= base_url()?>contacto" onsubmit="return onSubmit();" enctype="multipart/form-data">
                 <div class="col-lg-6">
                     <div class="form-group">
                         <label for="nombre">Nombre</label>
@@ -23,30 +23,31 @@
                         <?php echo form_error('comentario'); ?>
                     </div>
                     <div class="actions text-center">
-                        <button type="submit" value="Enviar" class="btn btn-success " name="submit"><span class="glyphicon glyphicon-floppy-saved"></span>Enviar</button>&nbsp;
+                        <button type="submit" value="Submit" class="btn btn-success"><span class="glyphicon glyphicon-floppy-saved"></span>Enviar</button>&nbsp;
                         <a href="<?= base_url()?>contacto"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-floppy-remove"></span> Cancelar</button></a>
                     </div>
 
                 </div>
-                <div class="col-sm-offset-2 col-lg-4">
-                    <label>Directora:</label>
-                    <p class="text-justified">Mag. Emma Patricia Victorio Cánovas</p>
-                    <label>Jefe de Administración:</label>
-                    <p class="text-justified">Sra. Norma Alpún Villarroel</p>
-                    <label>Telefono principal:</label>
-                    <p class="text-justified">619-7000</p>
-                    <ul>
-                        <li>DIRECCIÓN: Anexo 7531</li>
-                        <li>ADMINISTRACIÓN: Anexo 7529/7531</li>
-                        <li>VENTAS: Anexo 7530/7534</li>
-                    </ul>
-
-                    <label>Direccion:</label>
-                    <p class="text-justified">Jr. Germán Amezaga S/N, Biblioteca Central
-                        "Pedro Zulen",Cuarto Piso</p>
-                    <p class="text-center">Ciudad Universitaria Lima - Perú</p>
-                </div>
             </form>
+            <div class="col-sm-offset-2 col-lg-4">
+                <label>Directora:</label>
+                <p class="text-justified">Mag. Emma Patricia Victorio Cánovas</p>
+                <label>Jefe de Administración:</label>
+                <p class="text-justified">Sra. Norma Alpún Villarroel</p>
+                <label>Telefono principal:</label>
+                <p class="text-justified">619-7000</p>
+                <ul>
+                    <li>DIRECCIÓN: Anexo 7531</li>
+                    <li>ADMINISTRACIÓN: Anexo 7529/7531</li>
+                    <li>VENTAS: Anexo 7530/7534</li>
+                </ul>
+
+                <label>Direccion:</label>
+                <p class="text-justified">Jr. Germán Amezaga S/N, Biblioteca Central
+                    "Pedro Zulen",Cuarto Piso</p>
+                <p class="text-center">Ciudad Universitaria Lima - Perú</p>
+            </div>
+
         </div>
     </div>
 
--- js/enviado.js
+++ js/enviado.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
-    $(document).on("click", ".enviado", function() {
-        var exito = "<?php if(isset($_POST[submit]))?>";
-        if (exito) {
-            bootbox.dialog({
-                message: "Su mensaje fue enviado correctamente",
-                title: "MENSAJE ENVIADO",
-                buttons: {
-                    success: {
-                        label: "Ok!",
-                        className: "btn-success",
-                    }
-                }
-            });
+function onSubmit(){
+     bootbox.dialog({
+        message: "Su mensaje fue enviado correctamente",
+        title: "MENSAJE ENVIADO",
+        buttons: {
+            success: {
+                label: "Ok!",
+                className: "btn-success",
+            }
         }
-
     });
-});
+}
