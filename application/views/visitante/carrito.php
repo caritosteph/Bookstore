91339a38c4a856d8c73a11c434c94961eddcda9b
sadhu89 <carlos.sadhu@gmail.com>
2013-12-18 13:27:15 -0500

    carrito fijo

--- application/views/visitante/carrito.php
+++ application/views/visitante/carrito.php
@@ -1,6 +1,6 @@
   <div class="container">
 
-    <div class="row well">
+    <div class="row well" id="centrar">
       <div class="col-md-12 text-center">
         <h4>Para agregar libros al carrito de compras, debes estas registrado o iniciar sesion.</h4>
         <hr>
