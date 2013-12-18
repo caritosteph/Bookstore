5dcdb1f43e8776fa9af22898863f2414167fe05d
caritosteph <10200194@unmsm.edu.pe>
2013-12-18 16:10:48 -0500

    Mejoras

--- application/views/visitante/recuperarClave.php
+++ application/views/visitante/recuperarClave.php
@@ -1,7 +1,19 @@
-Ingrese correo para mandarle un enlace para reestablecer su contraseña
-<br>
-<form action="<?= base_url().'cliente/enviarCorreoClave' ?>" method="post">
-    Email <input type="email" name="email" />
-    <br>
-    <input type="submit" value="Enviar" />
-</form>
+<div class="container">
+
+    <div class="row formulario">
+        <div class="col-sm-offset-4 col-sm-4">
+            <h2 class="text-center">RESTABLECER CONTRASEÑA</h2>
+            <br>
+            <form action="<?= base_url().'cliente/enviarCorreoClave' ?>" method="post">
+                <div class="form-group ">
+                    <label for="email">Correo Electrónico</label>
+                    <input type="email" class="form-control" id="email_1" name="email" value="<?= set_value('email'); ?>" placeholder="Correo Electrónico" required>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary ">Enviar</button>
+                </div>                
+
+            </form>
+        </div>
+    </div>
+</div>
